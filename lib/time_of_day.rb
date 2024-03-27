c7ce3765a2726bd1ef4577c81289e111e49f6dd2
benmelz <ben@melz.me>
2024-03-27 10:00:11 -0400

    bundle gem .

---
+++ lib/time_of_day.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "time_of_day/version"
+
+module TimeOfDay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/time_of_day_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TimeOfDay do
+  it "has a version number" do
+    expect(TimeOfDay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
